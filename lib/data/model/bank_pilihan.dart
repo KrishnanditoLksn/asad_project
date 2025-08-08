60708f4e2a2dd1bd34699e2e203cfebdab2f9378
KrishnanditoLksn <krislaksana123@gmail.com>
2025-08-08 19:26:46 +0700

    feat:bank and history model

---
+++ lib/data/model/bank_pilihan.dart
@@ -0,0 +1,7 @@
+class BankPilihan {
+  String? namaBank;
+  String? nominal;
+  String? minTransaksi;
+  
+  BankPilihan({required this.namaBank, this.nominal , required this.minTransaksi});
+}
---
+++ lib/data/model/history_transaksi.dart
@@ -0,0 +1,11 @@
+class HistoryTransaksi {
+  String? jenis;
+  String? tanggal;
+  String? pengeluaran;
+
+  HistoryTransaksi({
+    required this.jenis,
+    required this.tanggal,
+    required this.pengeluaran,
+  });
+}
