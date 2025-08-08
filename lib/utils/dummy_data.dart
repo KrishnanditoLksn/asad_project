4c6b0ee3dc59c24b4322a64948e839f9af1e90a0
KrishnanditoLksn <krislaksana123@gmail.com>
2025-08-08 19:27:04 +0700

    feat:Added dummy datas

--- lib/utils/dummy_data.dart
+++ lib/utils/dummy_data.dart
@@ -1,4 +1,6 @@
+import 'package:asad_project/data/model/bank_pilihan.dart';
 import 'package:asad_project/data/model/berita.dart';
+import 'package:asad_project/data/model/history_transaksi.dart';
 
 List<Berita> dummyNews = [
   Berita(
@@ -17,4 +19,39 @@ List<Berita> dummyNews = [
     title: "Perayaan Hari Santri tampak berbeda",
     date: "31/10/2023",
   ),
+];
+
+// List lengkap berdasarkan data transaksi dari gambar
+List<HistoryTransaksi> history = [
+  HistoryTransaksi(
+    jenis: "Penarikan",
+    tanggal: "09 Mar 2022",
+    pengeluaran: "Rp 300.000",
+  ),
+  HistoryTransaksi(
+    jenis: "Setoran",
+    tanggal: "09 Mar 2022",
+    pengeluaran: "Rp 200.000",
+  ),
+  HistoryTransaksi(
+    jenis: "Setoran",
+    tanggal: "09 Mar 2022",
+    pengeluaran: "Rp 500.000",
+  ),
+  HistoryTransaksi(
+    jenis: "Penarikan",
+    tanggal: "09 Mar 2022",
+    pengeluaran: "Rp 100.000",
+  ),
+  HistoryTransaksi(
+    jenis: "Penarikan",
+    tanggal: "09 Mar 2022",
+    pengeluaran: "Rp 150.000",
+  ),
+];
+
+List<BankPilihan> bankPilihan = [
+  BankPilihan(namaBank: "Bank BRI", minTransaksi: "100.000,00"),
+  BankPilihan(namaBank: "Bank BCA", minTransaksi: "50.000,00"),
+  BankPilihan(namaBank: "Bank BNI", minTransaksi: "50.000,00"),
 ];
